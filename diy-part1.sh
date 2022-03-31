65c3189f512a1ece53720e09fe26eb563c2a9c08
fshh1988 <276355108@qq.com>
2022-03-31 11:28:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ pushd package/mypackage
 
 git clone https://github.com/sypopo/luci-theme-atmaterial
 git clone https://github.com/immortalwrt-collections/openwrt-gowebdav
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone https://github.com/tty228/luci-app-adguardhome
 
 # Add luci-app-ssr-plus
 #git clone --depth=1 https://github.com/fw876/helloworld
